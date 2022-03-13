2944bd138a1a6002d4f26b9adf8b9a2dc5a6659d
Fran <franngoesa@gmail.com>
2022-03-13 12:21:58 -0300

    updated sign logic

--- src/main/java/org/moon/moonfix/mixin/SignBlockEntityMixin.java
+++ src/main/java/org/moon/moonfix/mixin/SignBlockEntityMixin.java
@@ -1,9 +1,6 @@
 package org.moon.moonfix.mixin;
 
 import net.minecraft.block.entity.SignBlockEntity;
-import net.minecraft.item.DyeItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.moon.moonfix.config.ConfigManager.Config;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,12 +14,10 @@ public class SignBlockEntityMixin {
 
     @Shadow private boolean editable;
 
-    //edit signs on right click
+    //edit signs on right click with an empty hand while sneaking
     @Inject(at = @At("HEAD"), method = "onActivate", cancellable = true)
     public void onActivate(ServerPlayerEntity player, CallbackInfoReturnable<Boolean> cir) {
-        ItemStack itemStack = player.getStackInHand(player.getActiveHand());
-
-        if (!(boolean) Config.SIGN_EDIT.value || itemStack.getItem() instanceof DyeItem || itemStack.isOf(Items.GLOW_INK_SAC) || itemStack.isOf(Items.INK_SAC))
+        if (!(boolean) Config.SIGN_EDIT.value || !player.isSneaking() || !player.getStackInHand(player.getActiveHand()).isEmpty())
             return;
 
         this.editable = true;
@@ -30,6 +25,5 @@ public class SignBlockEntityMixin {
         player.openEditSignScreen(sign);
 
         cir.setReturnValue(true);
-        cir.cancel();
     }
 }
